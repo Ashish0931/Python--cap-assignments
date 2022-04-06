b9c907ee64e04cebfb7231601e1167d4e661f45a
Ashish0931 <40832481+Ashish0931@users.noreply.github.com>
2022-04-06 21:32:20 +0530

    Add files via upload

---
+++ calc_if_func.py
@@ -0,0 +1,26 @@
+from calendar import calendar
+
+
+def calc(num1,num2):
+    condition=True
+    while(condition==True):
+
+        if selector=="+":
+            print(num1+num2)
+            condition=False
+        elif (selector=="-"):
+            print(num1-num2)
+            condition=False
+        elif(selector=="*"):
+            print(num1*num2)
+            condition=False
+        elif(selector=="/"):
+            print("num1/num2")
+            condition=False
+        else:
+            print("wrong value entered! try again! ")
+            condition=True
+selector = input("enter the action you want to perform: (+,-,*,/)")
+num1=int(input("enter number 1: "))
+num2=int(input("enter number 2: "))
+calc(num1,num2)
